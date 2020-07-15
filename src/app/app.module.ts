1d8866b93c37aa83d09fa68d7287fe2eb8f87b91
kevinruse <kevin@kevinruse.com>
2020-07-14 19:30:08 -0700

    challenge exercise: create a component with the angular cli tool

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 
 @NgModule({
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent,
+                  HeaderComponent ],
   imports: [
     BrowserModule,
     CommonModule
