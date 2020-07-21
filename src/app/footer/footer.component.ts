3a31f7b7cd9587eec468a4c996692f9c4cce1b90
kevinruse <kevin@kevinruse.com>
2020-07-21 10:05:46 -0700

    guided exercise setting a property binding

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   versionString: string = '1.0.0';
+  icon: string = 'assets/images/icons/icons-29.png';
   versionNumber: number = this.versionStringToNumber(this.versionString);
 
   private versionStringToNumber(version: string): number {
