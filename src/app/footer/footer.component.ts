4dd48af541ed5651f7e84be52eb6d559eacb4c3d
kevinruse <kevin@kevinruse.com>
2020-07-21 10:14:54 -0700

    guided exercise adding a class and attribbute binding

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -10,6 +10,7 @@ export class FooterComponent implements OnInit {
   versionString: string = '1.0.0';
   icon: string = 'assets/images/icons/icons-29.png';
   logoAlt: string = 'FoodPlate logo';
+  isCurrent: boolean = true;
   versionNumber: number = this.versionStringToNumber(this.versionString);
 
   private versionStringToNumber(version: string): number {
