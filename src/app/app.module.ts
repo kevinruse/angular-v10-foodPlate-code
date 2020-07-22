91b2a60778b37f32742d27ff587edfe4523c023f
kevinruse <kevin@kevinruse.com>
2020-07-22 14:14:05 -0700

    challenge exercise create parent/child relationships between components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { HomeBtnComponent } from './home-btn/home-btn.component';
 
 @NgModule({
   declarations: [ AppComponent,
                   HeaderComponent,
-                  FooterComponent ],
+                  FooterComponent,
+                  MainComponent,
+                  HomeBtnComponent ],
   imports: [
     BrowserModule,
     CommonModule
