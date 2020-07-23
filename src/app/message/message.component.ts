3fc560d82f398ab6cbd4d58dbc1d476977ef2376
kevinruse <kevin@kevinruse.com>
2020-07-23 09:05:57 -0700

    guided exercise using @child to access a native element

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'fp-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
-export class MessageComponent implements OnInit {
+export class MessageComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('firstnameElement') firstnameElementRef: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.firstnameElementRef.nativeElement.style.backgroundColor = 'lightblue';
+    console.log(this.firstnameElementRef);
   }
 
 }
