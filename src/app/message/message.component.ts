a6f488c978e6a0c6d1e21ca8a1c44f09762096bb
kevinruse <kevin@kevinruse.com>
2020-08-03 09:26:46 -0700

    challenge exercise: add an ngif to the foodplate-cli application

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'fp-message',
@@ -9,14 +10,37 @@ export class MessageComponent implements OnInit, AfterViewInit {
 
   @ViewChild('firstnameElement') firstnameElementRef: ElementRef;
 
+  @Input()
+  user: User;
+
+  groupsMet = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
-    this.firstnameElementRef.nativeElement.style.backgroundColor = 'lightblue';
-    console.log(this.firstnameElementRef);
+    // this.firstnameElementRef.nativeElement.style.backgroundColor = 'lightblue';
+    // console.log(this.firstnameElementRef);
   }
 
+  getFoodGroupsMet(): string {
+    let messageString = `You have eaten all your `;
+
+    if (this.user.reqsStatus.fruitMet === true) {
+      this.groupsMet.push('fruit');
+    }
+    if (this.user.reqsStatus.vegMet === true) {
+      this.groupsMet.push(' vegetables');
+    }
+    if (this.user.reqsStatus.proteinMet === true) {
+      this.groupsMet.push(' protein');
+    }
+    if (this.user.reqsStatus.grainMet === true) {
+      this.groupsMet.push(' grains');
+    }
+    messageString += this.groupsMet.toString();
+    return messageString;
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,7 +8,7 @@ import { UserStatusService } from './user-status.service';
 })
 export class UserService {
   private user: User = new User(1, 'Kevin', 'M', '51+', 'M51+', {},
-      {fruitMet:  false, vegMet: false, proteinMet: false, grainMet: false},
+      {fruitMet:  true, vegMet: false, proteinMet: true, grainMet: false},
       false, 'kevin@kevinruse.com');
 
   getUser(): User {
