c4cddde8faeb4ee170fcc40d3b2b60292553196b
kevinruse <kevin@kevinruse.com>
2020-08-18 11:00:45 -0700

    guided exercise create and use a custom pipe

--- src/app/food-groups/food-detail/food-detail.module.ts
+++ src/app/food-groups/food-detail/food-detail.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DoublePipe } from '../../shared/double.pipe';
 import { FruitDetailComponent } from './fruit-detail/fruit-detail.component';
 import { GrainsDetailComponent } from './grains-detail/grains-detail.component';
 import { ProteinDetailComponent } from './protein-detail/protein-detail.component';
@@ -8,7 +9,11 @@ import { VegetableDetailComponent } from './vegetable-detail/vegetable-detail.co
 
 
 @NgModule({
-  declarations: [FruitDetailComponent, GrainsDetailComponent, ProteinDetailComponent, VegetableDetailComponent],
+  declarations: [FruitDetailComponent,
+                 GrainsDetailComponent,
+                 ProteinDetailComponent,
+                 VegetableDetailComponent,
+                 DoublePipe],
   imports: [
     CommonModule
   ]
