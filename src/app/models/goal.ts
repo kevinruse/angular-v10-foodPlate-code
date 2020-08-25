ec1906e0b1c970d4a0127eacb6188d269727bdcb
kevinruse <kevin@kevinruse.com>
2020-08-25 10:29:05 -0700

    add goal.ts data model

--- src/app/models/goal.ts
+++ src/app/models/goal.ts
@@ -1,3 +1,6 @@
-/**
- * Created by Kevin Ruse + Associates Inc. on 8/25/2020.
- */
+export class Goal {
+    id: number = 0;
+    deadline: string;
+    didIt: boolean = false;
+    goalTitle: string;
+}
