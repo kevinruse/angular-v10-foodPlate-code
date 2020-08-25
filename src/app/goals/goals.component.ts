8060867236c0897c14d2a5b0baa21e37e637c1d7
kevinruse <kevin@kevinruse.com>
2020-08-25 11:07:03 -0700

    guided exercise create the service with the crud operations

--- src/app/goals/goals.component.ts
+++ src/app/goals/goals.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Goal } from '../models/goal';
 import { GoalsService } from '../services/goals.service';
 
 @Component({
   selector: 'fp-goals',
-  templateUrl: 'goals.component.html',
-  styleUrls: ['goals.component.css']
+  templateUrl: './goals.component.html',
+  styleUrls: ['./goals.component.css']
 })
-
 export class GoalsComponent implements OnInit {
   goalForm: FormGroup;
   allGoals;
-  errorMessage: string;
-  isLoading: boolean;
   goal: Goal;
+  activeGoal: Goal;
+  newGoalView = false;
+  isLoading;
+  errorMessage: string;
+
 
   createForm(): void {
     this.goalForm = this.fb.group({
@@ -37,9 +39,96 @@ export class GoalsComponent implements OnInit {
           console.log(this.allGoals);
           this.isLoading = false;
         }, err => {
-          this.errorMessage = err;
-          console.log(this.errorMessage = err.message);
+          console.log(err);
           this.isLoading = false;
         });
+  }
+
+  showEditGoalForm(goal: Goal): void {
+    this.newGoalView = true;
+    this.getGoal(goal.id);
+    this.showGoalAddEditForm(true);
+  }
+
+  toggleGoalComplete(goal: Goal): void {
+    goal.didIt = !goal.didIt;
+  }
+
+  showAddGoalForm(): void {
+    this.showGoalAddEditForm(true);
+    this.resetGoalService();
+  }
+
+  showGoalAddEditForm(showForm: boolean): void {
+    this.newGoalView = showForm;
+  }
+
+  getGoal(id): void {
+    this.goalsService.getGoalById(id)
+        .subscribe(
+            goal => this.goalRetrieved(goal),
+            error => {
+              this.errorMessage = error;
+              console.log(error);
+            }
+        );
+  }
+
+  deleteGoal(goal): void {
+    this.goalsService.deleteGoalById(goal.id)
+        .subscribe(goal => this.goalsService.getGoals());
+  }
+
+  goalRetrieved(goal): void {
+    this.goal = goal;
+    this.goalForm.setValue({
+      id: this.goal.id,
+      deadline: this.goal.deadline,
+      didIt: this.goal.didIt,
+      goalTitle: this.goal.goalTitle
+    });
+  }
+
+  resetGoalService(): void {
+    this.goalForm.reset();
+  }
+
+  deleteCompleted(): void {
+    const completedGoals = this.allGoals.filter(goals => goals.didIt === true).map(goals => this.deleteGoal(goals));
+    console.log(completedGoals);
+  }
+
+  insertGoal(goal: Goal): void {
+    this.goalsService.addGoal(goal)
+        .subscribe(goal => {
+              this.goalsService.getGoals();
+            },
+            (error) => console.log(error)
+        );
+  }
+
+  updateGoal(goal: Goal): void {
+    this.goalsService.updateGoal(goal)
+        .subscribe(goal => this.goalsService.getGoals());
+  }
+
+  toggleAccomplished(): void {
+    console.log(`toggleAccomplished called`);
+    this.goalForm.patchValue({didIt: true});
+  }
+
+  submitGoal(goal): void {
+    console.log(`submitGoal() called`);
+    if (this.goalForm.invalid) {
+      console.log('submitGoal(): this.goalForm.invalid = true');
+      return;
+    }
+    this.showGoalAddEditForm(false);
+    // Insert
+    if (goal.id === null || goal.id < 1) {
+      this.insertGoal(goal);
+    } else {    // Update
+      this.updateGoal(goal);
+    }
   }
 }
